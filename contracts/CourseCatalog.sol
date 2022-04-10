eb8d4fdce7e5cc03b97ba8edc0df49f571e2f62a
Denes <denes710@gmail.com>
2022-04-10 14:42:17 +0200

    progress: CourseCatalog with university

--- contracts/CourseCatalog.sol
+++ contracts/CourseCatalog.sol
@@ -1,56 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./Access/Ownable.sol";
+import "./Library/Common.sol";
 import "./Token/ERC1238/ERC1238.sol";
-
-import "./Common.sol";
+import "./Person.sol";
+import "./University.sol";
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-// FIXME comments
-contract CourseCatalog is Ownable, ERC1238 {
+/**
+ * @dev Implementation of the available courses which can be used during a semester.
+ */
+ contract CourseCatalog is ERC1238 {
     using Counters for Counters.Counter;
     using Common for Common.State;
 
-    Common.State private _currentState;
+    // mapping course with credit value
+    mapping(uint256 => uint8) public creditValues;
 
+    // last course unique id
     Counters.Counter private _tokenId;
+    // main university smart contract
+    University private _university;
 
-    mapping(uint256 => uint8) public _creditValues;
-
-    constructor(string memory name_, string memory symbol_) ERC1238(name_, symbol_) {
-        Common.init(_currentState);
+    constructor(string memory name_, string memory symbol_, address universityAddr_) ERC1238(name_, symbol_) {
+        _university = University(universityAddr_);
     }
 
-    function mint(address to, uint8 creditValue) onlyOwner offSeason public {
-        super._mint(to, _tokenId.current());
-        _creditValues[_tokenId.current()] = creditValue;
+    function mint(uint8 creditValue_) offSeason public {
+        require(_university.teacher().balanceOf(_msgSender()) == 1, "Only teacher can mint a course!");
         _tokenId.increment();
+        super._mint(_msgSender(), _tokenId.current());
+        creditValues[_tokenId.current()] = creditValue_;
     }
 
-    function burn(uint256 tokenId) onlyOwner offSeason public {
-        super._burn(tokenId);
-        delete _creditValues[tokenId];
+    function burn(uint256 tokenId_) offSeason public {
+        require(ERC1238.ownerOf(tokenId_) == _msgSender(), "Sender is not the owner!");
+        super._burn(tokenId_);
+        delete creditValues[tokenId_];
     }
 
-    function setTokenURI(uint256 tokenId, string memory tokenURI) modifiableState public {
-        super._setTokenURI(tokenId, tokenURI);
+    function setTokenURI(uint256 tokenId_, string memory tokenURI_) modifiableState public {
+        require(ERC1238.ownerOf(tokenId_) == _msgSender());
+        super._setTokenURI(tokenId_, tokenURI_);
     }
 
     modifier modifiableState() {
-        require(_currentState.current() == Common.EState.offSeason ||
-            _currentState.current() == Common.EState.planning,
+        require(_university.currentState() == Common.EState.offSeason ||
+            _university.currentState() == Common.EState.planning,
             "Course catalog cannot be modified in the current state!");
         _;
     }
 
     modifier offSeason() {
-        require(_currentState.current() == Common.EState.offSeason, "Course catalog is not in the off season!");
+        require(_university.currentState() == Common.EState.offSeason,
+            "Course catalog is not in the off season!");
         _;
-    }
-
-    function setNextState() onlyOwner public {
-        Common.nextState(_currentState);
     }
 }
